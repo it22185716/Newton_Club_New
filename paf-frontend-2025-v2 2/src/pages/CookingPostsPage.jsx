c561a0291989ba4f268522c1df2583a6de838afb
sasiniashani <it22264848@my.sliit.lk>
2025-05-10 21:11:35 +0530

    Update CookingPostsPage.jsx

--- paf-frontend-2025-v2 2/src/pages/CookingPostsPage.jsx
+++ paf-frontend-2025-v2 2/src/pages/CookingPostsPage.jsx
@@ -59,7 +59,7 @@ const CookingPostsPage = () => {
           onClick={openCreateModal}
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
         >
-          Create New Post
+          Create Meal Plan Post
         </button>
       </div>
 
